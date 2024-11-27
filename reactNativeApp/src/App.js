d1d6c8c19cb55db16f1302f166b71cb7bfd41de6
RubP04 <rubenruben1521@gmail.com>
2024-11-27 15:09:18 -0500

    hid navbar from main page

--- reactNativeApp/src/App.js
+++ reactNativeApp/src/App.js
@@ -1,11 +1,11 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import CategorySelection from './components/CategorySelection';
-import ForgotPassword from './components/ForgotPassword'; // Import ForgotPassword
+import ForgotPassword from './components/ForgotPassword';
 import NotFound from './components/NotFound';
 import Scraper from './components/Scraper';
 import NavigationBar from './components/NavigationBar';
@@ -13,20 +13,36 @@ import NavigationBar from './components/NavigationBar';
 const App = () => {
   return (
     <Router>
-      <NavigationBar />
+      <AppContent />
+    </Router>
+  );
+};
+
+const AppContent = () => {
+  const location = useLocation();
+
+  // Define paths where the NavigationBar should be hidden
+  const hideNavbarPaths = ['/legislative-entries'];
+
+  // Check if the current path is in the array of paths to hide the navbar
+  const shouldHideNavbar = hideNavbarPaths.includes(location.pathname);
+
+  return (
+    <>
+      {/* Conditionally render the NavigationBar */}
+      {!shouldHideNavbar && <NavigationBar />}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/category-selection" element={<CategorySelection />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} /> {/* Add route for ForgotPassword */}
-        <Route path="/legislative-entries" element={<Scraper />} /> {/* Add this route */}
-
-
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/legislative-entries" element={<Scraper />} />
         {/* Catch-all route for 404 */}
-       <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </Router>
+    </>
   );
 };
 
